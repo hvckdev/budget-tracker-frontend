56cd7f5a66971640ae10e95e96eb915a95627878
hvck <work@hvck.su>
2023-01-01 17:41:23 +0400

    💄 feat: Login and register forms

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,5 +1,22 @@
 <template>
-
+  <div>
+    <div class="card">
+      <div class="card-header">Login</div>
+      <div class="card-body">
+        <form>
+          <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-
+    <div class="card">
+      <div class="card-header">Registration</div>
+      <div class="card-body">
+        <form>
+          <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username">
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password">
+          </div>
+          <div class="mb-3">
+            <label for="password_confirmation" class="form-label">Confirm password</label>
+            <input type="password" class="form-control" id="password_confirmation">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
