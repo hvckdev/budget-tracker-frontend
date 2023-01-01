c1786498995d21e78a3b47c9fe085c01ddb38282
hvck <work@hvck.su>
2023-01-01 15:16:59 +0400

    🏗️ feat: Added bootstrap & configured others

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Navbar />
+    <div class="container mt-3">
+      <NuxtPage />
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import Navbar from "~/partials/Navbar.vue";
+</script>
---
+++ partials/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <NuxtLink to="/" class="nav-link">Home</NuxtLink>
+            </li>
+          </ul>
+          <div class="d-flex">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <NuxtLink to="login" class="nav-link">Login</NuxtLink>
+              </li>
+              <li class="nav-item">
+                <NuxtLink to="register" class="nav-link">Register</NuxtLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
