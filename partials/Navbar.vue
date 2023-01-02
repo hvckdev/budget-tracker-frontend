cdd4dd29005ec5d015ab8ac69285a2a03ba9f61a
hvck <work@hvck.su>
2023-01-02 18:53:38 +0400

    🐛 fix: Homepage link

--- partials/Navbar.vue
+++ partials/Navbar.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">😍 Budget Tracker</a>
+        <NuxtLink class="navbar-brand" to="/">😍 Budget Tracker</NuxtLink>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
