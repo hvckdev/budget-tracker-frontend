5c18bd95c6d4e70d2d7d59a75b14d2dac1d814f7
hvck <work@hvck.su>
2023-01-06 14:29:55 +0400

    💄 feat: Some improvements

--- app.vue
+++ app.vue
@@ -1,12 +1,21 @@
+<script setup lang="ts">
+import Navbar from "~/partials/Navbar.vue";
+
+const mounted = ref(false);
+
+onMounted(() => {
+  mounted.value = true;
+})
+
+</script>
+
 <template>
   <div>
-    <Navbar />
-    <div class="container mt-3">
-      <NuxtPage />
+    <div v-if="mounted">
+      <Navbar />
+      <div class="container mt-3">
+        <NuxtPage />
+      </div>
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-import Navbar from "~/partials/Navbar.vue";
-</script>
--- plugins/useAuth.client.ts
+++ plugins/useAuth.client.ts
@@ -3,7 +3,7 @@ import useUser from "~/composables/useUser";
 export default defineNuxtPlugin((nuxtApp) => {
     const { fetchUser } = useUser();
 
-    nuxtApp.hook('app:mounted', () => {
-        fetchUser().then();
+    nuxtApp.hook('app:created', async () => {
+        await fetchUser();
     })
 })
