3e384f7ccda9162ae5d24b980dacfaaba453bd80
hvck <work@hvck.su>
2023-01-06 14:09:43 +0400

    🪧 feat: Information about purchase

--- pages/purchases/[purchase]/index.vue
+++ pages/purchases/[purchase]/index.vue
@@ -1,13 +1,47 @@
-<template>
+<script setup>
+import usePurchases from '~/composables/usePurchases';
+import moment from 'moment';
 
-</template>
+const { params } = useRoute();
+const { getPurchase } = usePurchases();
 
-<script>
-export default {
-  name: 'index',
+const purchase = ref(null);
+
+onMounted(async () => { purchase.value = await getPurchase(params.purchase); });
+
+const formatDate = (date) => {
+  return moment(date).format('Do MMMM YYYY');
 };
+
 </script>
-
-<style scoped>
-
-</style>
+<template>
+  <div>
+    <div v-if="purchase" class="card">
+      <div class="card-header">
+        <span class="h3">{{ purchase.name }}</span>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col">
+            <p class="h5">About</p>
+            <p>
+              Amount:
+              <strong>{{ purchase.amount }} RUB</strong>
+            </p>
+            <p>Made: {{ formatDate(purchase.created_at) }}</p>
+          </div>
+          <div class="col">
+            <p class="h5">Products</p>
+            <ul class="list-group">
+              <li class="list-group-item" v-for="product in purchase.products">
+                <p>Name: {{ product.name }}</p>
+                <p>Price: {{ product.price }} RUB</p>
+                <p>Category: {{ product.categoryResource.name }}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
